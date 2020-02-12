e9e9ceba5fcf2b88010d02f28a9cb56ecff24ae3
vnpro149 <57343987+vnpro149@users.noreply.github.com>
2020-02-12 18:11:24 +0700

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,4 @@
+APICEM_IP = "10.215.26.9" #Địa chỉ IP của APIC-EM
+USERNAME ="vnpro" # Tên đăng nhập trong APIC-EM
+PASSWORD ="VnPro123" # Mật khẩu của tài khoản APIC-EM
+VERSION = "v1" # Phiên bản APIC-EM version 1
