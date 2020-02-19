10df23418c657d13ea749c8cb66bbfcb64e6784e
vnpro149 <57343987+vnpro149@users.noreply.github.com>
2020-02-19 11:13:45 +0700

    Delete controller.py

--- controller.py
+++
@@ -1,4 +0,0 @@
-APICEM_IP = "10.215.26.9" #Địa chỉ IP của APIC-EM
-USERNAME ="vnpro" # Tên đăng nhập trong APIC-EM
-PASSWORD ="Vnpro123" # Mật khẩu của tài khoản APIC-EM
-VERSION = "v1" # Phiên bản APIC-EM version 1
