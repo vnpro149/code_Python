bb7cad56debeb43fdc1912baf20808b34c8e73f5
vnpro149 <57343987+vnpro149@users.noreply.github.com>
2020-02-19 09:52:05 +0700

    Update delete_device.py

--- delete_device.py
+++ delete_device.py
@@ -6,18 +6,20 @@ from tabulate import tabulate
 import pgpd
 import get_network_device
 
+#Lay so id cua thiet bi
 def get_device_id():
     inputString = input("Nhap so thu tu cua thiet bi can xoa:")
-    number =0
     device =[]
     resp = pgpd.get(api="network-device")
     response_json = resp.json() 
     device = response_json["response"]
+    
+    number = 0
     for item in device:
         number +=1
         if number==int(inputString):
-            id_selete = item["id"]
-    return id_selete
+            id_seleted = item["id"]
+    return id_seleted
 #Xoa thiet bi
 def delete_device():
     id = get_device_id()
@@ -30,8 +32,13 @@ def delete_device():
     except:
         print("Something wrong")
         sys.exit()
-#Lay danh sach cac thiet bi
-result = get_network_device.network_device_list()
-print(tabulate(result,
+
+def main():
+    #Lay danh sach cac thiet bi
+    result = get_network_device.network_device_list()
+    print(tabulate(result,
             headers = ['number','hostname','ip','type','mac address','id'], tablefmt="rst"))
-result2 = delete_device()
+    result2 = delete_device()
+
+if __name__ == '__main__':
+    sys.exit(main())
