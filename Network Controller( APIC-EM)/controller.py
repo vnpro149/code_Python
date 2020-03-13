7352920d7684e2c2d2817236b27c07748aebcc06
hotuanhoanh8 <55940179+hotuanhoanh8@users.noreply.github.com>
2020-03-13 11:36:59 +0700

    1

--- Network Controller( APIC-EM)/controller.py
+++ Network Controller( APIC-EM)/controller.py
@@ -2,4 +2,3 @@ APICEM_IP = "10.215.26.9" #Địa chỉ IP của APIC-EM
 VERSION = "v1" # Phiên bản APIC-EM version 1
 USERNAME ="vnpro" # Tên đăng nhập trong APIC-EM
 PASSWORD ="Vnpro123" # Mật khẩu của tài khoản APIC-EM
-###
